d423a7f56d1321c52128528a68052ad122d6f5b3
maripau07@gmail.com <maripau07@gmail.com@1ce589b3-49d6-3e2a-c15d-7297e91df399>
2012-05-10 03:29:00 +0000

    Generalization Refactoring

--- trunk/edu.unicen.ucrefactoring/src/edu/unicen/ucrefactoring/refactorings/GeneralizationRefactoring.java
+++ trunk/edu.unicen.ucrefactoring/src/edu/unicen/ucrefactoring/refactorings/GeneralizationRefactoring.java
@@ -1,11 +1,17 @@
 package edu.unicen.ucrefactoring.refactorings;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import edu.unicen.ucrefactoring.analyzer.AlignmentX2Result;
 import edu.unicen.ucrefactoring.analyzer.SimilarBlock;
+import edu.unicen.ucrefactoring.gui.UCRUseCasesView;
 import edu.unicen.ucrefactoring.metrics.Metric;
+import edu.unicen.ucrefactoring.model.Event;
 import edu.unicen.ucrefactoring.model.Flow;
+import edu.unicen.ucrefactoring.model.InclusionCall;
+import edu.unicen.ucrefactoring.model.UCRefactoringFactory;
 import edu.unicen.ucrefactoring.model.UseCase;
 
 public class GeneralizationRefactoring implements Refactoring {
@@ -25,7 +31,7 @@ public class GeneralizationRefactoring implements Refactoring {
 	
 	@Override
 	public boolean canApply() {
-		// TODO INCORPORAR CHEQUEO DE IGUALES FLUJOS ALTERNATIVOS / EXT 
+		// TODO INCORPORAR CHEQUEO DE IGUALES FLUJOS ALTERNATIVOS / EXT. TB Q LOS AL MENOS UNO NO TENGA PADRE
 		if ((this.alignment.getFlowA().getName().equals("Basic Flow") 
 			&& this.alignment.getFlowB().getName().equals("Basic Flow"))
 			&& (this.alignment.getSimilarBlocksFromA().size()>1
@@ -45,8 +51,110 @@ public class GeneralizationRefactoring implements Refactoring {
 		 * 4-
 		 * 
 		 */
+		UseCase baseUseCaseA = null;
+		UseCase baseUseCaseB = null;
+		UseCase parentUC = null;
+		if (!this.isThisParent(this.alignment.getFlowA()) && !isThisParent(this.alignment.getFlowB())){
+			baseUseCaseA = this.alignment.getUseCaseA();
+			baseUseCaseB = this.alignment.getUseCaseB();
+			// Create Use Case
+			// TODO MAKE THE USER COMPLETE THE NAME AND DESCRIPTION
+			parentUC = UCRefactoringFactory.eINSTANCE.createUseCase();
+			parentUC.setName("Default Name");
+			parentUC.setDescription("Default Description");
+			parentUC.setPrimaryActor(null); // TODO ABSTRAER ACTORES
+			Flow basicFlow = UCRefactoringFactory.eINSTANCE.createFlow();
+			basicFlow.setName("Basic Flow");
+			Integer order = 1;
+			Boolean lastParticular = false;
+			List<Event> allDuplicated = new ArrayList<Event>();
+			for (SimilarBlock sb :this.alignment.getSimilarBlocksFromA()){
+				allDuplicated.addAll(sb.getSimilarEvents());
+			}
+			for (Event actualEv : baseUseCaseA.getBasicFlow().getEvents()){
+				if(allDuplicated.contains(actualEv)){
+					Event newE = actualEv.cloneEvent();
+					newE.setEventId(actualEv.getEventId().replaceFirst(actualEv.getNumber().toString(), order.toString()));
+					newE.setNumber(order);
+					basicFlow.getEvents().add(newE);
+					order++;
+					lastParticular = false;
+				}
+				else if (!lastParticular){
+					InclusionCall newE = UCRefactoringFactory.eINSTANCE.createInclusionCall();
+					newE.setEventId(order + ".");
+					newE.setNumber(order);
+					newE.setDetail("Delegated to particular implementation");
+					basicFlow.getEvents().add(newE);
+					order++;
+					lastParticular = true;
+				}
+			}
+			basicFlow.setUseCase(parentUC);
+			parentUC.getFlows().add(basicFlow);
+			// Add new uc to the model
+			UCRUseCasesView.ucref.getUseCaseModel().getUseCases().add(parentUC);
+		}
+		else{
+			if (isThisParent(this.alignment.getFlowA())){
+				baseUseCaseB = this.alignment.getUseCaseB();
+				parentUC = this.alignment.getUseCaseA();
+			}
+			else{
+				baseUseCaseA =this.alignment.getUseCaseA();
+				parentUC = this.alignment.getUseCaseB();
+			}
+		}
+		if(baseUseCaseA != null){
+			this.editBaseFlow(this.alignment.getSimilarBlocksFromA(), parentUC);
+		}
+		if(baseUseCaseB != null){
+			this.editBaseFlow(this.alignment.getSimilarBlocksFromB(), parentUC);
+		}
+	}
+	
+	private void editBaseFlow(List<SimilarBlock> simBlockToRemove, UseCase parent){
+		// Edit the Basic Flow
+		UseCase baseUC = simBlockToRemove.get(0).getUseCase();
+		Flow basicFlow = baseUC.getBasicFlow();
+		List<Event> allDuplicated = new ArrayList<Event>();
+		for (SimilarBlock sb :simBlockToRemove){
+			allDuplicated.addAll(sb.getSimilarEvents());
+		}
+		Boolean lastAbstract = false;
+		List<Event> newList = new ArrayList<Event>();
+		for (Event e : basicFlow.getEvents()){
+			if (allDuplicated.contains(e)){
+				// Remove this abstract event
+				if (!lastAbstract){
+					InclusionCall newE = UCRefactoringFactory.eINSTANCE.createInclusionCall();
+					Integer last = newList.size();
+					newE.setNumber(last+1);
+					newE.setEventId(last+1+".");
+					newE.setDetail("Common behaviour defined in parent use case");
+					newList.add(newE);
+					lastAbstract = true;
+				}
+			}
+			else{
+				// it is particular, leave it here
+				Integer last = newList.size();
+				e.setNumber(last+1);
+				e.setEventId(last+1+".");
+				newList.add(e);
+				lastAbstract = false;
+			}
+		}
+		basicFlow.getEvents().clear();
+		basicFlow.getEvents().addAll(newList);
+		baseUC.setParent(parent);
 	}
 
+	public Boolean isThisParent(Flow f){
+		// TODO VER SI UNO ES PADRE, ES DECIR SI YA FORMA PARTE DE UNA JERARQUIA 
+		return false;
+	}
+	
 	@Override
 	public Float getScore() {
 		if (this.score == null){
