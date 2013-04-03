86b86afec1ecc18aa355d70ef41775aaeed825ec
miguebt@gmail.com <miguebt@gmail.com@1ce589b3-49d6-3e2a-c15d-7297e91df399>
2013-04-03 19:12:37 +0000

    Fixed long use cases scoring function.

--- edu.unicen.ucrefactoring/src/edu/unicen/ucrefactoring/refactorings/ExtractUseCaseRefactoring.java
+++ edu.unicen.ucrefactoring/src/edu/unicen/ucrefactoring/refactorings/ExtractUseCaseRefactoring.java
@@ -130,9 +130,9 @@ public class ExtractUseCaseRefactoring implements Refactoring {
 			Float basicCoef = (float)(1 - (((float)LargeUseCaseMetric.BASIC_FLOW_LIMIT/((LargeUseCaseMetric)metric).getUseCaseBasicEventsSize(this.useCase))));
 			
 			if (this.score == null){
-				totalCoef = (totalCoef<0)?1:totalCoef;
-				basicCoef = (basicCoef<0)?1:basicCoef;
-				Float confiability = (float)(totalCoef / basicCoef)/2;
+				totalCoef = (totalCoef<0)?0:totalCoef;
+				basicCoef = (basicCoef<0)?0:basicCoef;
+				Float confiability = (float)(totalCoef + basicCoef)/2;
 				//getScore
 				this.score = (confiability*(0.4f)+this.getPriority()*(0.6f))*100;
 			}
